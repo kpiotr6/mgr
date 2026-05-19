e4aeea378fdccffd0d41e789c769e0944ced7468
kpiotr6 <piotrek@komlan.pl>
2026-05-19 18:25:48 +0200

    Hope it works fixed

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 #SBATCH --job-name=tsf
 #SBATCH --time=48:00:00
-#SBATCH --account=plggwykrywanieanom
+#SBATCH --account=plgmedanomaly-gpu
 #SBATCH --partition=plgrid-gpu-v100
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
 #SBATCH --gres=gpu
 
 module add GCCcore/14.3.0 Python/3.13.5
-export PYTHONPATH="${PYTHONPATH}:/net/tscratch/people/plgkpiotr6/pip"
+export PYTHONPATH="${PYTHONPATH}:/net/afscra/people/plgkpiotr6/pip"
 python model_training/train_models.py
